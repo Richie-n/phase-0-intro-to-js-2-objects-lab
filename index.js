72931801369993d703586f71b994f254595e28b3
Richard <ngangarichard335@gmail.com>
2024-03-30 04:03:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Richard',
+    streetAddress: 'Kikuyu',
+}
+console.log(employee)
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    return{
+        ...obj,
+        [key]:value,
+    }
+
+}
+const newEmployee = updateEmployeeWithKeyAndValue(employee,"name","Njoroge")
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key]= value
+    return obj
+}
+const otherEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee,"streetAddress","Karen")
+
+function deleteFromEmployeeByKey(obj, key) {
+    const  newObj= { ...obj };
+
+        delete newObj[key];
+      
+
+    return newObj;
+}
+const firedEmployee =deleteFromEmployeeByKey(employee,"name")
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+        delete obj[key];
+        return obj
+}
+const lastEmployee = destructivelyDeleteFromEmployeeByKey(employee, "streeAddress")
+    
+
+
+
+
+
